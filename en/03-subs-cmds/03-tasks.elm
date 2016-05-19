46593da84f3016f1d912c54330e21cc504d5b391
Sebastian Porto <sebasporto@gmail.com>
2016-05-19 21:45:00 +1000

    Add task example

--- en/03-subs-cmds/03-tasks.elm
+++ en/03-subs-cmds/03-tasks.elm
@@ -3,6 +3,9 @@ module Main exposing (..)
 import Html exposing (Html, div, button, text)
 import Html.Events exposing (onClick)
 import Html.App
+import Http
+import Task
+import Json.Decode as Decode
 
 -- MODEL
 
@@ -16,6 +19,8 @@ init =
 
 type Msg
   = Fetch
+  | FetchSuccess String
+  | FetchError Http.Error
 
 -- VIEW
 
@@ -25,12 +30,28 @@ view model =
     [ button [ onClick Fetch ] [ text "Fetch" ]
     , text model ]
 
+decode : Decode.Decoder String
+decode =
+  Decode.at ["name"] Decode.string
+
+fetch : Cmd Msg
+fetch =
+  let
+    url =
+      "http://swapi.co/api/planets/1/"
+  in
+    Task.perform FetchError FetchSuccess (Http.get decode url)
+
 -- UPDATE
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Fetch ->
+      (model, fetch)
+    FetchSuccess name ->
+      (name, Cmd.none)
+    FetchError _ ->
       (model, Cmd.none)
 
 -- SUBSCRIPTIONS
